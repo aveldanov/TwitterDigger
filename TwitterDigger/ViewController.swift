0ffb00dd22b4cfc70aca0974e9df73af19ee148d
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-17 22:39:23 -0700

    first commit

---
+++ TwitterDigger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterDigger
+//
+//  Created by Veldanov, Anton on 4/17/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
