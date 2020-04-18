3e7b243e2094799a8d23c03c98bc9680b2c8dcdd
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-17 23:05:34 -0700

    iOS

--- TwitterDigger/ViewController.swift
+++ TwitterDigger/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet weak var backgroundView: UIView!
+  
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBOutlet weak var sentimentLabel: UILabel!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
 
+  
+  
+  @IBAction func predictPressed(_ sender: UIButton) {
+  }
+  
+  
 }
 
