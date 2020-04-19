08e345c20743a7dda99d237c86afe27ae6ec10ea
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-18 19:45:24 -0700

    iOS

---
+++ TwitterDigger/Keys.swift
@@ -0,0 +1,15 @@
+//
+//  Keys.swift
+//  TwitterDigger
+//
+//  Created by Veldanov, Anton on 4/18/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Keys {
+  let APIkey: String = "IcuS7CrFGnOdqW8TysTfIKHWN"
+  let APISecretKey: String = "3zyxGrCBBkQvRJjwwehZ0pRgn4UnQXGwUxUyjs3VR1kgHqo6jt"
+}
--- TwitterDigger/ViewController.swift
+++ TwitterDigger/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController {
 
   
   @IBOutlet weak var backgroundView: UIView!
-  
   @IBOutlet weak var textField: UITextField!
-  
   @IBOutlet weak var sentimentLabel: UILabel!
   
+  let swifter = Swifter(consumerKey: TWITTER_CONSUMER_KEY, consumerSecret: TWITTER_CONSUMER_SECRET)
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
