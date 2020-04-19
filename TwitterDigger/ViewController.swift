512e1a7729d5e3d3fd3975ccdde70b2b37f9c32a
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-18 22:44:46 -0700

    iOS

--- TwitterDigger/ViewController.swift
+++ TwitterDigger/ViewController.swift
@@ -12,7 +12,7 @@ import CoreML
 import SwiftyJSON
 
 class ViewController: UIViewController {
-
+  
   let keys = Keys()
   
   let sentimentClassifier = TwitterDiggerClassification()
@@ -21,32 +21,55 @@ class ViewController: UIViewController {
   @IBOutlet weak var textField: UITextField!
   @IBOutlet weak var sentimentLabel: UILabel!
   
-
+  
   var swifter: Swifter{
     return Swifter(consumerKey: keys.APIkey, consumerSecret: keys.APISecretKey)
   }
-
-
+  
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-//      print(results)
+    swifter.searchTweet(using: "#blessed", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+      //      print(results)
       
-      var tweets = [String]()
+      var tweets = [TwitterDiggerClassificationInput]()
       for i in 0..<100{
         if let tweet = results[i]["full_text"].string{
-           
-//           print(tweet)
-          tweets.append(tweet)
-           
-         }
+          
+          //           print(tweet)
+          let tweetForClassification = TwitterDiggerClassificationInput(text: tweet)
+          tweets.append(tweetForClassification)
+          
+        }
         
       }
       
-// print(tweets)
-      
+      // print(tweets)
+      do{
+        
+       let predicatons = try self.sentimentClassifier.predictions(inputs: tweets)
+       
+        var sentimentScore = 0
+        
+        for pred in predicatons{
+          let sentiment = pred.class_
+          
+          if sentiment == "Pos"{
+            sentimentScore += 1
+          }else if sentiment == "Neg"{
+            sentimentScore -= 1
+          }
+          
+        }
+        
+//        predicatons.map{print($0.class_)}
+        
+        print(sentimentScore)
+      }catch{
+        print(error)
+      }
       
       
       
@@ -55,12 +78,16 @@ class ViewController: UIViewController {
     }
     
     
-    let predication = try! sentimentClassifier.prediction(text: "@Apple America love it best ever favorite")
     
-    print(predication.class_)
-
+    
+    
+    //    // check 1 tweet
+    //    let predication = try! sentimentClassifier.prediction(text: "@Apple America love it best ever favorite")
+    //
+    //    print(predication.class_)
+    
   }
-
+  
   
   
   
